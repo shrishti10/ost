89cdfda6ee89fb9738af4eca22e526e28da98c23
shrishti10 <32140852+shrishti10@users.noreply.github.com>
2017-10-31 03:58:11 +0530

    Add files via upload

---
+++ room.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>ADMIN LOGIN</title>
+</head>
+<body background="http://localhost/ost/ro.png" style="color:blue;font-size:125%;text-align:center;">
+	<form action="room1.php" method="POST">
+			<fieldset>
+				<legend>
+					ADMINISTRATIVE TASK
+				</legend>
+				<p>DO YOU WANT TO ADD NEW ROOM?<br><input type="submit" value="CLICK HERE TO ADD NEW ROOM" name="aroom"></p>
+				<br>
+				<p>DO YOU WANT TO DELETE ROOM?<br><input type="submit" value="CLICK HERE TO DELETE ANY ROOM" name="droom"></p>
+				<br>
+				<p>DO YOU WANT TO CHECK ROOM STATUS?<br><input type="submit" value="CLICK HERE TO CHECK THE STATUS OF ROOMS" name="vacant"></p>
+				<br>
+				<p>DO YOU WANT TO COUNT VACANT ROOM?<br><input type="submit" value="CLICK HERE TO COUNT VACANT ROOM" name="countroom"></p>
+				<br>
+			</fieldset>
+	</form>
+</body>
+</html>
